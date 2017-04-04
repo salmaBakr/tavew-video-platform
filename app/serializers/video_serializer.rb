761fb5537d70726a77211b559282f77379715c3c
johnram528 <johnram528@gmail.com>
2017-04-04 17:10:49 -0400

    added url instance method to serializer

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,6 +1,11 @@
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :title, :file_content_type, :file_file_name, :file_file_size, :created_at, :description
+  attributes :id, :title, :url, :file_content_type, :file_file_name, :file_file_size, :created_at, :description
   belongs_to :user
+
+  def url 
+    object.file.url
+  end
+
 end
 
 

147517c3d2e077a4910b17737c356dd17ce30733
johnram528 <johnram528@gmail.com>
2017-04-04 13:06:45 -0400

    added user validations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates_presence_of :username, :email  
+  validates_uniqueness_of :username, :email
+  has_secure_password
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+end
+
+

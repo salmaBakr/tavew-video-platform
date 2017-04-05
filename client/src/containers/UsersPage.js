f39979c8f3008f1fced6fb2923a3f9af82ddc932
johnram528 <johnram528@gmail.com>
2017-04-04 23:44:24 -0400

    added initial fetch to UsersPage

--- client/src/containers/UsersPage.js
+++ client/src/containers/UsersPage.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 export default class UsersPage extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('/api/users/1/')
+      .then(response =>response.json())
+      .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div>UsersPage is Coming</div>

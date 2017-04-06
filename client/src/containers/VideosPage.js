7bd0b790c7e404af8844ae07818a32c942660361
johnram528 <johnram528@gmail.com>
2017-04-06 15:35:53 -0400

    filled in VideosPage component with old UsersPage code

--- client/src/containers/VideosPage.js
+++ client/src/containers/VideosPage.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import '../index.css'
 
 export default class VideosPage extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+    this.fetchVideos.bind(this)
+  }
+
+  componentDidMount(){
+    this.fetchVideos()
+  }
+
+  fetchVideos() {
+    fetch('/api/videos')
+      .then(response =>response.json())
+      .then(data => this.setState({
+        videos: data.videos
+      })
+      )
+  }
+
   render() {
-    return (
-      <div>VideosPage is Coming</div>
+    const childrenWithProps = React.Children.map(this.props.children, (child) =>
+      React.cloneElement(child,{
+        fetchVideos: this.fetchVideos
+      }))
+
+    const videos = this.state.videos.map( (video) =>
+        
+        <div key={video.id} className="vidContainer">
+          <h2>{video.title}</h2>
+          <Link to="/users/1/videos/2">
+            <video width="360" height="240" className="thumbVid">
+              <source src={video.url} type={video.type}/>
+            </video>
+          </Link>
+        </div>
+  
+      )
+
+    return (      
+      <div>
+      
+      
+      <Link to="/users/1/videos/new">Upload New Video</Link>
+      {childrenWithProps || videos}
+      
+
+      
+      </div>
       )
   }
 }

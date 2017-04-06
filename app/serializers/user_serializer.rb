6592df4a81e50bd7a5961565797973ec951fe6fb
johnram528 <johnram528@gmail.com>
2017-04-06 15:46:45 -0400

    commented out user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :bio, :created_at
-  has_many :videos
-end
+#User Model is pending
+
+# class UserSerializer < ActiveModel::Serializer
+#   attributes :id, :username, :email, :bio, :created_at
+#   has_many :videos
+# end

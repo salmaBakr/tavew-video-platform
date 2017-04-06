8dac680a0feb1197bc156cfadf227a5d9165c6e7
johnram528 <johnram528@gmail.com>
2017-04-06 15:45:45 -0400

    commented out user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
-class User < ApplicationRecord
-  has_many :videos 
+#User model is pending
 
-  validates_presence_of :username, :email  
-  validates_uniqueness_of :username, :email
-  has_secure_password
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-end
+# class User < ApplicationRecord
+#   has_many :videos 
+
+#   validates_presence_of :username, :email  
+#   validates_uniqueness_of :username, :email
+#   has_secure_password
+#   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+# end
 
 

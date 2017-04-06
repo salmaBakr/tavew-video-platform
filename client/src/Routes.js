abcacde5ccf4fe0a54cfc3fbe5698c444eb51351
johnram528 <johnram528@gmail.com>
2017-04-06 15:36:50 -0400

    substitued UsersPage for VideosPage in Routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
-import UsersPage from './containers/UsersPage'
+import VideosPage from './containers/VideosPage'
 import VideosNew from './containers/VideosNew'
 import VideosShow from './containers/VideosShow'
 import Home from './components/Home'
@@ -9,7 +9,7 @@ import Home from './components/Home'
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
-    <Route path="/users/:id" component={UsersPage}> 
+    <Route path="/users/:id" component={VideosPage}> 
       <Route path="/users/:id/videos/new" component={VideosNew}/> 
       <Route path="/users/:id/videos/:videoId" component={VideosShow}/> 
     </Route>

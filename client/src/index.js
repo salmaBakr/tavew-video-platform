e93969e444c916ed5959ecb445374a86d59e9cd6
johnram528 <johnram528@gmail.com>
2017-04-07 00:19:37 -0400

    connected provider in index, added thunk, moved store to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router' 
-import store from './configureStore';
 import { Provider } from 'react-redux'; 
 import Routes from './Routes.js'
+import {createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 import './index.css';
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <Router history={browserHistory} routes={Routes}/>,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={Routes}/>
+    </Provider>,
   document.getElementById('root')
 );

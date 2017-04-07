e1b155bee0676ca54f9e3b1921b0121aaf19cf46
johnram528 <johnram528@gmail.com>
2017-04-07 00:20:35 -0400

    added thunk to store

--- client/src/index.js
+++ client/src/index.js
@@ -10,8 +10,11 @@ import './index.css';
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>

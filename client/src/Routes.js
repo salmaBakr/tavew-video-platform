7e58f5f93107418ae682b34e3b9989028c279887
johnram528 <johnram528@gmail.com>
2017-04-06 22:22:25 -0400

    added provider to routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -5,14 +5,17 @@ import VideosPage from './containers/VideosPage'
 import VideosNew from './containers/VideosNew'
 import VideosShow from './containers/VideosShow'
 import Home from './components/Home'
+import { Provider } from 'react-redux'; 
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={VideosPage}/>
-    <Route path="/videos" component={VideosPage}> 
-      <Route path="/videos/new" component={VideosNew}/> 
-      <Route path="/videos/:videoId" component={VideosShow} modal={true}/> 
+  <Provider store={store}>
+    <Route path="/" component={App}>
+      <IndexRoute component={VideosPage}/>
+      <Route path="/videos" component={VideosPage}> 
+        <Route path="/videos/new" component={VideosNew}/> 
+        <Route path="/videos/:videoId" component={VideosShow} modal={true}/> 
+      </Route>
     </Route>
-  </Route>
+  </Provider>
   )
 

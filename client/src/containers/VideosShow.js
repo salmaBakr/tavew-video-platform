bedaf443f13bc4ff6d07a46279664932ec740109
johnram528 <johnram528@gmail.com>
2017-04-29 11:52:42 -0400

    added progress bar

--- client/src/containers/VideosShow.js
+++ client/src/containers/VideosShow.js
@@ -87,8 +87,11 @@ import { browserHistory } from 'react-router'
   render() { 
     let overlay =  (<div className='vid-overlay'>
               <button className='backButton' onClick={() => this.back()}><i className="fa fa-angle-left fa-3x" aria-hidden="true"></i></button>
-              <button className='playButton' onClick={() => this.play()}><i className= {this.state.playing ? 'fa fa-pause' : 'fa fa-play'} aria-hidden="true"></i></button>
-              {this.props.video.duration}
+              <div className='controls'>
+                <button className='playButton' onClick={() => this.play()}><i className= {this.state.playing ? 'fa fa-pause' : 'fa fa-play'} aria-hidden="true"></i></button>
+                <progress id='progress-bar' min='0' max='100' value='0'>0% played</progress>  
+                {this.props.video.duration}
+              </div>
             </div>)
  
     return (      
